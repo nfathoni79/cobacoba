45399350de2f7aad192a1028f86f82afe95272b3
NN Fathoni <naufalfathoni79@gmail.com>
2023-09-04 05:26:52 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mx-auto max-w-screen-md h-screen p-8">
+    <div class="w-full h-full bg-[url('/img/tree.jpg')]
+      bg-center bg-contain bg-no-repeat">
+    </div>
+  </div>
+</template>
